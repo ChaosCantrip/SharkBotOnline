1826a369fc0237592bf5aea58cba48728f9d9400
ChaosCantrip <dev@chaoscantrip.com>
2023-12-07 16:55:52 +0000

    Added /bungie_auth/success

---
+++ src/app/bungie_auth/success/page.js
@@ -0,0 +1,10 @@
+export default function BungieAuthSuccess(){
+    return (
+        <div>
+            <h1>Success!</h1>
+            <div>
+                <p>You can close this page!</p>
+            </div>
+        </div>
+    )
+}
