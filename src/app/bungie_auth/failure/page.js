cb407066bb88f6ecc72e18f9cb5262271bc56595
ChaosCantrip <dev@chaoscantrip.com>
2023-12-07 16:55:58 +0000

    Added /bungie_auth/failure

---
+++ src/app/bungie_auth/failure/page.js
@@ -0,0 +1,11 @@
+export default function BungieAuthFailure(){
+    return (
+        <div>
+            <h1>Oops! Something went Wrong!</h1>
+            <div>
+                <p>I've logged the error, but it would be super if you could shoot <b>ChaosCantrip</b> a message that would be great!</p>
+                <p>Or e-mail me over at <a href="mailto:dev@chaoscantrip.com">dev@chaoscantrip.com</a> that's more your thing.</p>
+            </div>
+        </div>
+    )
+}
