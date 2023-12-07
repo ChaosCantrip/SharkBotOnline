c7908492e6cbf320eb1d3a5c1d456921e0623c4d
ChaosCantrip <dev@chaoscantrip.com>
2023-12-07 18:40:23 +0000

    Added placeholder items, collections, leaderboards and members pages

---
+++ src/app/collections/page.js
@@ -0,0 +1,12 @@
+import article from "@styles/article.module.css";
+
+export default function CollectionsPage() {
+    return (
+        <div className={article.wrapper}>
+            <h1 className={article.title}>Collections Page</h1>
+            <div className={article.body}>
+                <p>Some content idk</p>
+            </div>
+        </div>
+    )
+}
