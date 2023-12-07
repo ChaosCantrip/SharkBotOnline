053d7c8829ba8ff0dd7d8b99bd99bd7b621afcba
ChaosCantrip <dev@chaoscantrip.com>
2023-12-07 17:57:55 +0000

    Assigned colours to body, Header, Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css";
 
 export default function Footer() {
     return (
-        <footer>
+        <footer className={styles.footer}>
             <h1>Footer</h1>
         </footer>
     )
