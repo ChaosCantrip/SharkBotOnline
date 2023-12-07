0a5063f622fd78a50f5a2496da484844065d5cf6
ChaosCantrip <dev@chaoscantrip.com>
2023-12-07 17:14:01 +0000

    Added /bungie_auth/maintenance

---
+++ src/app/bungie_auth/maintenance/page.js
@@ -0,0 +1,11 @@
+export default function BungieAuthMaintenance(){
+    return (
+        <div>
+            <h1>The Bungie API is currently down for maintenance!</h1>
+            <div>
+                <p>Unfortunately, this means that SharkBot is unable to authorize you at this time.</p>
+                <p>Please try again later.</p>
+            </div>
+        </div>
+    )
+}
