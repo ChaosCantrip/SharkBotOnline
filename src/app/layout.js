eb37c7f3d37c56a108e6eaca819bf9f64afeb973
ChaosCantrip <dev@chaoscantrip.com>
2023-12-07 17:51:46 +0000

    Added blank Header and Footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,15 @@
 
 import "@styles/master.css";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body>
+            <Header />
             {children}
+            <Footer />
             </body>
         </html>
     )
