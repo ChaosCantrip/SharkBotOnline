489f32a6ca33739d6e9a004c5a5284d281504337
ChaosCantrip <dev@chaoscantrip.com>
2023-12-07 18:12:33 +0000

    Added Header Title Stylings

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import styles from "./Header.module.css";
 export default function Header() {
     return (
         <header className={styles.header}>
-            <h1>Header</h1>
+            <div className={styles.title_wrapper}>
+                <h1 className={styles.title}>SharkBot Online</h1>
+            </div>
         </header>
     )
 }
