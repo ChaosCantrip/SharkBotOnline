e5d993a5f43c1586bab5f8f4a77de5cb6183b0ed
ChaosCantrip <dev@chaoscantrip.com>
2023-12-07 18:23:44 +0000

    Added basic footer contents

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <h1>Footer</h1>
+            <p>Copyright ChaosCantrip or something idk</p>
         </footer>
     )
 }
