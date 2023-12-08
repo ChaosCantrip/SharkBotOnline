ae157def25bfbaf7e8998e57a348605508e168e1
ChaosCantrip <dev@chaoscantrip.com>
2023-12-08 18:44:34 +0000

    Added member page metadata

--- src/app/members/[member_id]/page.js
+++ src/app/members/[member_id]/page.js
@@ -2,6 +2,31 @@ import article from "@styles/article.module.css";
 import {get_member} from "@lib/sharkbot";
 import {notFound} from "next/navigation";
 
+export async function generateMetadata({ params }) {
+    const member_id = params.member_id;
+    const member = await get_member(member_id);
+
+    if (!member) {
+        notFound();
+    }
+
+    return {
+        title: member.display_name,
+        description: `Profile for ${member.display_name} on SharkBot Online`,
+        images: [
+            {
+                url: member.avatar_url,
+                width: 256,
+                height: 256,
+                alt: member.display_name
+            }
+        ],
+        icons: {
+            icon: member.avatar_url
+        }
+    }
+}
+
 export default async function MemberPage({ params }) {
     const member_id = params.member_id;
     const member = await get_member(member_id);
