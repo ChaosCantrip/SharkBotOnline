8e4bd6d5161147dac48c9044e6fa2875d85e2f3a
ChaosCantrip <dev@chaoscantrip.com>
2023-12-08 17:01:02 +0000

    Changed collection_id, leaderboard_id, member_id to upper case

--- src/app/collections/[collection_id]/page.js
+++ src/app/collections/[collection_id]/page.js
@@ -1,7 +1,7 @@
 import article from "@styles/article.module.css";
 
 export default async function CollectionPage({ params }) {
-    const collection_id = params.collection_id;
+    const collection_id = params.collection_id.toUpperCase();
     return (
         <div className={article.wrapper}>
             <h1 className={article.title}>Collection: {collection_id}</h1>
--- src/app/members/[member_id]/page.js
+++ src/app/members/[member_id]/page.js
@@ -1,7 +1,7 @@
 import article from "@styles/article.module.css";
 
 export default function MemberPage({ params }) {
-    const member_id = params.member_id;
+    const member_id = params.member_id.toUpperCase();
     return (
         <div className={article.wrapper}>
             <h1 className={article.title}>Member: {member_id}</h1>
