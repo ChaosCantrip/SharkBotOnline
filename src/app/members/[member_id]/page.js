f2cafaf311fa2b83f9e55ccfbb42bd18570099bc
ChaosCantrip <dev@chaoscantrip.com>
2023-12-08 18:41:09 +0000

    Added member data fetching

--- src/app/members/[member_id]/page.js
+++ src/app/members/[member_id]/page.js
@@ -1,12 +1,21 @@
 import article from "@styles/article.module.css";
+import {get_member} from "@lib/sharkbot";
+import {notFound} from "next/navigation";
 
-export default function MemberPage({ params }) {
-    const member_id = params.member_id.toUpperCase();
+export default async function MemberPage({ params }) {
+    const member_id = params.member_id;
+    const member = await get_member(member_id);
+
+    if (!member) {
+        notFound();
+    }
+
     return (
         <div className={article.wrapper}>
             <h1 className={article.title}>Member: {member_id}</h1>
             <div className={article.body}>
                 <p>Some content idk</p>
+                <pre>{JSON.stringify(member, null, 4)}</pre>
             </div>
         </div>
     )
