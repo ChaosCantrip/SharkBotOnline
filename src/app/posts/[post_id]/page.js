5c00afbe69baf7f2e9f28a63929b03c983210069
ChaosCantrip <dev@chaoscantrip.com>
2023-12-14 09:16:29 +0000

    Added post displaying and footnotes

--- src/app/posts/[post_id]/page.js
+++ src/app/posts/[post_id]/page.js
@@ -1,12 +1,27 @@
 import article from "@styles/article.module.css";
+import {get_post} from "@lib/sharkbot";
+import {notFound} from "next/navigation";
+
+import styles from "./post.module.css";
 
 export default async function PostPage({ params }) {
     const post_id = params.post_id.toLowerCase();
+    const post = await get_post(post_id);
+
+    if (!post) {
+        notFound();
+    }
+
     return (
         <div className={article.wrapper}>
-            <h1 className={article.title}>Post: {post_id}</h1>
+            <h1 className={article.title}>{post.title}</h1>
             <div className={article.body}>
-                <p>Some content idk</p>
+                <p>{post.body}</p>
+            </div>
+            <div className={styles.footnotes}>
+                <p>{post.author.name}</p>
+                <p>{new Date(post.created_at).toString()}</p>
+                <p>{post.id}</p>
             </div>
         </div>
     )
