921185f3c15a83fae0c6e701bf8d04d917c50c86
ChaosCantrip <dev@chaoscantrip.com>
2023-12-14 08:35:53 +0000

    Added redirects for plurals

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: "/item/:path*",
+                destination: "/items/:path*",
+                permanent: true
+            },
+            {
+                source: "/collection/:path*",
+                destination: "/collections/:path*",
+                permanent: true
+            },
+            {
+                source: "/leaderboard/:path*",
+                destination: "/leaderboards/:path*",
+                permanent: true
+            },
+            {
+                source: "/member/:path*",
+                destination: "/members/:path*",
+                permanent: true
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
