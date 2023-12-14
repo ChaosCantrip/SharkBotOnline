0b6444918abd42537ddd7c6cd9230808bbb17402
ChaosCantrip <dev@chaoscantrip.com>
2023-12-14 09:23:24 +0000

    Added MDX to posts page

--- src/app/posts/[post_id]/page.js
+++ src/app/posts/[post_id]/page.js
@@ -1,6 +1,7 @@
 import article from "@styles/article.module.css";
 import {get_post} from "@lib/sharkbot";
 import {notFound} from "next/navigation";
+import { MDXRemote } from "next-mdx-remote/rsc"
 
 import styles from "./post.module.css";
 
@@ -16,7 +17,7 @@ export default async function PostPage({ params }) {
         <div className={article.wrapper}>
             <h1 className={article.title}>{post.title}</h1>
             <div className={article.body}>
-                <p>{post.body}</p>
+                <MDXRemote source={post.body} />
             </div>
             <div className={styles.footnotes}>
                 <p>{post.author.name}</p>
