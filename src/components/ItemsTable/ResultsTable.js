1f03f58ec93fae866177520d3f7a3be1e349300c
ChaosCantrip <dev@chaoscantrip.com>
2023-12-16 23:30:26 +0000

    Added ResultsTable sorting

--- src/components/ItemsTable/ResultsTable.js
+++ src/components/ItemsTable/ResultsTable.js
@@ -1,15 +1,63 @@
+"use client"
+
+import {useEffect, useState} from "react";
+
 export default function ResultsTable({ results }){
+    const [sort, setSort] = useState("index");
+    const [asc, setAsc] = useState(true);
+    const [sortedResults, setSortedResults] = useState([...results]);
+
+    function sortResults(){
+        let sorted = [...results];
+        sorted.sort((a, b) => {
+            if (a[sort] < b[sort]) {
+                return asc ? -1 : 1;
+            }
+            if (a[sort] > b[sort]) {
+                return asc ? 1 : -1;
+            }
+            return 0;
+        });
+        setSortedResults([...sorted]);
+    }
+
+    function handleSort(e){
+        let new_sort = e.target.getAttribute("id");
+        if (new_sort === sort) {
+            setAsc(!asc);
+        } else {
+            setSort(new_sort);
+            setAsc(true);
+        }
+    }
+
+    useEffect(() => {
+        sortResults();
+    }, [sort, asc, results]);
+
+    function TableHeader({ id, active, children }){
+        return (
+            <th onClick={handleSort} id={id}>
+                {children}
+            </th>
+        )
+    }
+
     return (
         <div>
             <table>
                 <thead>
                     <tr>
-                        <th>Item ID</th>
-                        <th>Name</th>
+                        <TableHeader id="index" active={sort === "index"}>
+                            ID
+                        </TableHeader>
+                        <TableHeader id="name" active={sort === "name"}>
+                            Name
+                        </TableHeader>
                     </tr>
                 </thead>
                 <tbody>
-                    {results.map((item, index) => {
+                    {sortedResults.map((item, index) => {
                         return (
                             <tr key={index}>
                                 <td>{item.id}</td>
