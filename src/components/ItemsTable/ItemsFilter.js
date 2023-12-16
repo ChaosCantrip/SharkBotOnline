29537454596116d20e47b66ef4893b6f60eb685d
ChaosCantrip <dev@chaoscantrip.com>
2023-12-16 22:18:10 +0000

    Implemented initial form of ItemsFilter including working SearchBar

--- src/components/ItemsTable/ItemsFilter.js
+++ src/components/ItemsTable/ItemsFilter.js
@@ -1,7 +1,27 @@
-export default function ItemsFilter(/* { items, setResults, search_bar, filter_options } */) {
+import SearchBar from "@components/ItemsTable/SearchBar";
+import {useEffect, useState} from "react";
+import FilterOptions from "@components/ItemsTable/FilterOptions";
+
+export default function ItemsFilter({ items, setResults, search_bar, filter_options }) {
+    const [search, setSearch] = useState("");
+    const [filter, setFilter] = useState({});
+
+    function handleChange() {
+        let items_copy = [...items];
+        if (search !== "") {
+            items_copy = items_copy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()));
+        }
+        setResults([...items_copy]);
+    }
+
+    useEffect(() => {
+        handleChange();
+    }, [search, filter]);
+
     return (
         <div>
-            Items Filter
+            {search_bar && <SearchBar setSearch={setSearch} />}
+            {filter_options && <FilterOptions setFilter={setFilter} />}
         </div>
     )
 }
