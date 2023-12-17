f5cbaa639d21b27dc8df6fffb0440869ca82fc07
ChaosCantrip <dev@chaoscantrip.com>
2023-12-17 00:31:59 +0000

    Changed database to postgres

--- src/lib/sharkbot.js
+++ src/lib/sharkbot.js
@@ -1,80 +1,75 @@
-import { doc, getDoc, setDoc, getDocs, collection } from 'firebase/firestore';
-import { db } from './firebase';
-
-async function get_doc(collection, document_id) {
-  const docRef = doc(db, collection, document_id);
-  const docSnap = await getDoc(docRef);
-  if (docSnap.exists()) {
-    return docSnap.data();
-  } else {
-    return null;
-  }
-}
-
-async function set_doc(collection, document_id, data) {
-  const docRef = doc(db, collection, document_id);
-  await setDoc(docRef, data);
-}
+import { sql } from "@vercel/postgres";
 
 export async function get_item(item_id) {
-    return await get_doc('items', item_id);
+    const item = await sql`SELECT * FROM items WHERE id = ${item_id}`;
+    if (item.rowCount === 0) {
+        return null;
+    } else {
+        return item.rows[0];
+    }
 }
 
 export async function get_all_items() {
-    const items = [];
-    const querySnapshot = await getDocs(collection(db, "items"));
-    querySnapshot.forEach((doc) => {
-        items.push(doc.data());
-    });
-    return items;
-}
-
-export async function get_all_items_lite() {
-    const items_dict = await get_doc("lite", "items");
-    const items = [];
-    for (const item_id in items_dict) {
-        items.push(items_dict[item_id]);
-    }
-    items.sort((a, b) => {
-        return a.index - b.index;
-    });
-    return items;
+    const items = await sql`SELECT * FROM items`;
+    return items.rows;
 }
 
 export async function get_collection(collection_id) {
-    return await get_doc('collections', collection_id);
+    const collection = await sql`SELECT * FROM collections WHERE id = ${collection_id}`;
+    if (collection.rowCount === 0) {
+        return null;
+    } else {
+        return collection.rows[0];
+    }
+}
+
+export async function get_collection_items(collection_id) {
+    const items = await sql`SELECT * FROM items WHERE collection_id = ${collection_id}`;
+    return items.rows;
 }
 
 export async function get_all_collections() {
-    const collections = [];
-    const querySnapshot = await getDocs(collection(db, "collections"));
-    querySnapshot.forEach((doc) => {
-        collections.push(doc.data());
-    });
-    return collections;
+    const collections = await sql`SELECT * FROM collections`;
+    return collections.rows;
 }
 
 export async function get_member(member_id) {
-    return await get_doc('members', member_id);
+    const member = await sql`SELECT * FROM members WHERE id = ${member_id}`;
+    if (member.rowCount === 0) {
+        return null;
+    } else {
+        return member.rows[0];
+    }
 }
 
 export async function get_leaderboard(leaderboard_id) {
-    return await get_doc('leaderboards', leaderboard_id);
+    const leaderboard = await sql`SELECT * FROM leaderboards WHERE id = ${leaderboard_id}`;
+    if (leaderboard.rowCount === 0) {
+        return null;
+    } else {
+        return leaderboard.rows[0];
+    }
 }
 
 export async function get_post(post_id) {
-    return await get_doc('posts', post_id);
+    const post = await sql`SELECT * FROM posts WHERE id = ${post_id}`;
+    if (post.rowCount === 0) {
+        return null;
+    } else {
+        return post.rows[0];
+    }
 }
 
 export async function set_post(post_id, data) {
-    return await set_doc('posts', post_id, data);
+    const post = await sql`SELECT * FROM posts WHERE id = ${post_id}`;
+    if (post.rowCount === 0) {
+        await sql`INSERT INTO posts (id, title, content, author, created_at, updated_at) VALUES (${data.id}, ${data.title}, ${data.content}, ${data.author}, ${data.created_at}, ${data.updated_at})`;
+    } else {
+        await sql`UPDATE posts SET title = ${data.title}, content = ${data.content}, author = ${data.author}, updated_at = ${data.updated_at} WHERE id = ${post_id}`;
+    }
 }
 
 export async function get_all_posts() {
-    const posts = [];
-    const querySnapshot = await getDocs(collection(db, "posts"));
-    querySnapshot.forEach((doc) => {
-        posts.push(doc.data());
-    });
-    return posts;
+    const posts = await sql`SELECT * FROM posts`;
+    return posts.rows;
 }
