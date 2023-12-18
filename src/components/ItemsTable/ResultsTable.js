e3cffcc5bbb4c39557587fa314b714059a7897fc
ChaosCantrip <dev@chaoscantrip.com>
2023-12-18 18:52:11 +0000

    Improved ResultsTable stylings

--- src/components/ItemsTable/ResultsTable.js
+++ src/components/ItemsTable/ResultsTable.js
@@ -1,23 +1,29 @@
 "use client"
 
 import {useEffect, useState} from "react";
+import styles from "./ResultsTable.module.css";
+import Link from "next/link";
 
 const columns_map = {
-    "id": {
-        "id": "index",
-        "name": "ID"
+    id: {
+        id: "index",
+        name: "ID",
+        className: styles.item_id
     },
-    "name": {
-        "id": "name",
-        "name": "Name"
+    name: {
+        id: "name",
+        name: "Name",
+        className: styles.item_name
     },
-    "description": {
-        "id": "description",
-        "name": "Description"
+    description: {
+        id: "description",
+        name: "Description",
+        className: styles.item_description
     },
-    "type": {
-        "id": "type",
-        "name": "Type"
+    type: {
+        id: "type",
+        name: "Type",
+        className: styles.item_type
     }
 }
 
@@ -56,16 +62,35 @@ export default function ResultsTable({ results, columns }){
 
     function TableHeader({ id, active, children }){
         return (
-            <th onClick={handleSort} id={id}>
+            <th onClick={handleSort} id={id} className={styles.table_header}>
                 {children}
             </th>
         )
     }
 
+    function TableCell({ id, children }) {
+        if (id === "name") {
+            return (
+                <td className={`${styles.item_cell} ${columns_map[id].className}`}>
+                    <Link href={`/items/${id.toUpperCase()}`}>
+                        {children}
+                    </Link>
+                </td>
+            )
+        } else {
+            return (
+                <td className={`${styles.item_cell} ${columns_map[id].className}`}>
+                    {children}
+                </td>
+            )
+        }
+    }
+
+
     return (
-        <div>
-            <table>
-                <thead>
+        <div className={styles.wrapper}>
+            <table className={styles.table}>
+                <thead className={styles.thead}>
                     <tr>
                         {
                             columns.map((column, index) => {
@@ -78,17 +103,19 @@ export default function ResultsTable({ results, columns }){
                         }
                     </tr>
                 </thead>
-                <tbody>
+                <tbody className={styles.tbody}>
                     {sortedResults.map((item, index) => {
                         return (
-                            <tr key={index}>
-                                {columns.map((column, index) => {
-                                    return (
-                                        <td key={index}>
-                                            {item[column]}
-                                        </td>
-                                    )
-                                })}
+                            <tr key={index} className={styles.item_row}>
+                                {
+                                    columns.map((column, index) => {
+                                        return (
+                                            <TableCell key={index} id={column}>
+                                                {item[column]}
+                                            </TableCell>
+                                        )
+                                    })
+                                }
                             </tr>
                         )
                     })}
