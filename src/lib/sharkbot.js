157d875b1f9f0db07eac51734fbceca5d47028be
ChaosCantrip <dev@chaoscantrip.com>
2023-12-18 09:36:18 +0000

    Added set_collection()

--- src/lib/sharkbot.js
+++ src/lib/sharkbot.js
@@ -53,6 +53,17 @@ export const get_all_collections = cache(async () => {
     return collections.rows;
 });
 
+export async function set_collection(collection_id, data) {
+    console.log(`DB WRITE | set_collection | collection_id: ${collection_id}`)
+    const collection = get_collection(collection_id);
+    if (collection === null) {
+        await sql`INSERT INTO collections (id, name, sharkcoin_value, xp_value, icon_url, index) VALUES (${data.id}, ${data.name}, ${data.sharkcoin_value}, ${data.xp_value}, ${data.icon_url}, ${data.index})`;
+    } else {
+        await sql`UPDATE collections SET name = ${data.name}, sharkcoin_value = ${data.sharkcoin_value}, xp_value = ${data.xp_value}, icon_url = ${data.icon_url}, index = ${data.index} WHERE id = ${collection_id}`;
+    }
+
+}
+
 /* ===== MEMBERS ===== */
 
 export const get_member = cache(async (member_id) => {
