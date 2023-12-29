4af9f1c95cc151c0ca8e79a63b90532397e29713
ChaosCantrip <dev@chaoscantrip.com>
2023-12-29 14:21:38 +0000

    Removed /api/items/lite

--- src/app/api/items/lite/route.js
+++
@@ -1,7 +0,0 @@
-import {get_all_items, get_all_items_lite} from "@lib/sharkbot";
-import {SharkResponse} from "@/app/api/SharkResponse";
-
-export async function GET(request) {
-    const items = await get_all_items_lite();
-    return SharkResponse({items: items});
-}
